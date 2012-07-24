de8717069a4dd2709d4401e34b9f1127546a006a
Chakradar Raju <chakradarraju@gmail.com>
2012-07-24 15:28:35 +0530

    Rearranging js

--- jscss/script.js
+++ jscss/script.js
@@ -1,56 +1,96 @@
+// Event Handlers
+
 $("li a").click(function() {
-    $("body").css("background","url('img/"+$(this).attr("href").substr(1)+"')");
     var lielement = $(this).parent();
     $(lielement).siblings().removeClass("active");
     $(lielement).addClass("active");
+    navBarHandler($(this).attr("href").substr(1));
 });
-$("#tweetBox").focus(function() {
-    $(this).prev().slideDown();
-    $(this).next().slideDown();
-});
-$("#tweetBox").blur(function() {
-    if($(this).val()=="") {
-        $(this).prev().slideUp();
-        $(this).next().slideUp();
-    }
-});
-resizeIt = function() {
-    var str = $('#tweetBox').val();
-    $('#tweetBox').attr('rows',str.split("\n").length+1);
-};
-$("#clearBtn").click(function() {
-    $("#tweetBox").fadeOut(function() {
-        $("#tweetBox").val("");
-        $("#tweetBox").focus();
-        resizeIt();
-        $("#tweetBox").fadeIn();
+
+function feedSetup() {
+    $("#tweetBox").focus(function() {
+        $(this).prev().slideDown();
+        $(this).next().slideDown();
     });
-});
-$("#tweetBox").keydown(resizeIt);
-resizeIt();
-$("#postBtn").click(function() {
-    $("#ticker").html("Posting tweet...");
-    $("#ticker").fadeIn();
-    $.post("/backend/post/",{
+    $("#tweetBox").blur(function() {
+        if($(this).val()=="") {
+            $(this).prev().slideUp();
+            $(this).next().slideUp();
+        }
+    });
+    $("#clearBtn").click(function() {
+        $("#tweetBox").fadeOut(function() {
+            $("#tweetBox").val("");
+            $("#tweetBox").focus();
+            resizeIt();
+            $("#tweetBox").fadeIn();
+        });
+    });
+    $("#postBtn").click(function() {
+        $("#ticker").html("Posting tweet...");
+        $("#ticker").fadeIn();
+        $.post("/backend/post/",{
             'postcontent':$("#tweetBox").val()
         }, function() {
-            $("#ticker").fadeOut(); 
+            $("#ticker").fadeOut();
             $("#tweetBox").fadeOut(function() {
                 $("#tweetBox").val("");
                 resizeIt();
                 $("#tweetBox").blur();
                 $("#tweetBox").fadeIn();
             });
+            updateFeed();
         });
-});
-$.get('/backend/feed/',{},function(data) {
-    var html = "";
-    for(tweetid in data['tweets']) {
-        var tweet = data['tweets'][tweetid];
-        html += "<div class='tweet'>";
-        html += "<span class='user'>"+tweet['sourceuser']+"</span><br/>";
-        html += tweet['postcontent'];
-        html += "</div>";
+    });
+    $("#tweetBox").keydown(resizeIt);
+}
+
+// functions
+
+function navBarHandler(href) {
+    if(href=="feed") {
+        updateFeed();
+        showFeed();
+        feedSetup();
+    } else if(href=="myprofile") {
+        updateProfile();
+        showProfile();
     }
-    $("#tweetsContainer").html(html);
-}, "json");
+}
+function resizeIt() {
+    var str = $('#tweetBox').val();
+    if(!str) str = "";
+    $('#tweetBox').attr('rows',str.split("\n").length+1);
+}
+function updateFeed() {
+    $.get('/backend/feed/',{},function(data) {
+        var html = "";
+        for(tweetid in data['tweets']) {
+            var tweet = data['tweets'][tweetid];
+            html += "<div class='tweet'>";
+            html += "<span class='user'>"+tweet['sourceuser']+"</span><br/>";
+            html += tweet['postcontent'];
+            html += "</div>";
+        }
+        $("#tweetsContainer").html(html);
+    }, "json");
+}
+function showFeed() {
+    resizeIt();
+    $("#content-wrapper").html($("#tmpl-feed").html());
+}
+function updateProfile() {
+    $.get('/backend/myprofile/',{},function(data) {
+        var html = "";
+
+    });
+}
+function showProfile() {
+    $("#content-wrapper").html($("#tmpl-profile").html());
+}
+
+// Bootup code
+
+updateFeed();
+showFeed();
+feedSetup();
