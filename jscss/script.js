a5503a8671e3d0d328df393b575e69e9a3309510
Chakradar Raju <chakradarraju@gmail.com>
2012-07-25 17:14:02 +0530

    Profile and feed working

--- jscss/script.js
+++ jscss/script.js
@@ -4,57 +4,70 @@ $("li a").click(function() {
     var lielement = $(this).parent();
     $(lielement).siblings().removeClass("active");
     $(lielement).addClass("active");
-    navBarHandler($(this).attr("href").substr(1));
+    var tab = $(this).attr("href").substr(1);
+    showTab(tab);
+    navBarHandler(tab);
 });
-
-function feedSetup() {
-    $("#tweetBox").focus(function() {
-        $(this).prev().slideDown();
-        $(this).next().slideDown();
+$("#logoutButton").click(function() {
+    $.post('/backend/logout/',{}, function(data) {
+        if(data['message']=="Successfully logged out")
+            window.location = "/frontend/login.html";
+    },"json");
+});
+$("#tweetBox").focus(function() {
+    $(this).prev().slideDown();
+    $(this).next().slideDown();
+});
+$("#tweetBox").blur(function() {
+    if($(this).val()=="") {
+        $(this).prev().slideUp();
+        $(this).next().slideUp();
+    }
+});
+$("#clearBtn").click(function() {
+    $("#tweetBox").fadeOut(function() {
+        $("#tweetBox").val("");
+        $("#tweetBox").focus();
+        resizeIt();
+        $("#tweetBox").fadeIn();
     });
-    $("#tweetBox").blur(function() {
-        if($(this).val()=="") {
-            $(this).prev().slideUp();
-            $(this).next().slideUp();
-        }
-    });
-    $("#clearBtn").click(function() {
+});
+$("#postBtn").click(function() {
+    $("#ticker").html("Posting tweet...");
+    $("#ticker").fadeIn();
+    $.post("/backend/post/",{
+        'postcontent':$("#tweetBox").val()
+    }, function() {
+        $("#ticker").fadeOut();
         $("#tweetBox").fadeOut(function() {
             $("#tweetBox").val("");
-            $("#tweetBox").focus();
             resizeIt();
+            $("#tweetBox").blur();
             $("#tweetBox").fadeIn();
         });
+        updateFeed();
     });
-    $("#postBtn").click(function() {
-        $("#ticker").html("Posting tweet...");
-        $("#ticker").fadeIn();
-        $.post("/backend/post/",{
-            'postcontent':$("#tweetBox").val()
-        }, function() {
-            $("#ticker").fadeOut();
-            $("#tweetBox").fadeOut(function() {
-                $("#tweetBox").val("");
-                resizeIt();
-                $("#tweetBox").blur();
-                $("#tweetBox").fadeIn();
-            });
-            updateFeed();
-        });
-    });
-    $("#tweetBox").keydown(resizeIt);
-}
+});
+$("#tweetBox").keydown(resizeIt);
+setInterval(refreshTimes, 10000);
 
 // functions
 
+function refreshTimes() {
+    $(".active .timestamp").each(function() {
+        var dur = new Date() - new Date(parseInt($(this).attr('ref')));
+        $(this).html(timeDiff(dur)+" ago");
+    });
+}
+function showTab(tab) {
+    $(".sector").removeClass("active");
+    $("#tab-"+tab).addClass("active");
+}
 function navBarHandler(href) {
     if(href=="feed") {
         updateFeed();
-        showFeed();
-        feedSetup();
-    } else if(href=="myprofile") {
+    } else if(href=="profile") {
         updateProfile();
-        showProfile();
     }
 }
 function resizeIt() {
@@ -62,35 +75,78 @@ function resizeIt() {
     if(!str) str = "";
     $('#tweetBox').attr('rows',str.split("\n").length+1);
 }
+function something(a) {
+    alert(a);
+    return timeDiff(new Date()-new Date(a.attr('href')));
+}
+function timeDiff(dur) {
+    dur = Math.ceil(dur/1000);
+    if(dur<60) return dur + " sec";
+    dur = Math.ceil(dur/60);
+    if(dur<60) return dur + " min";
+    dur = Math.ceil(dur/60);
+    if(dur<24) return dur + " hrs";
+    dur = Math.ceil(dur/24);
+    return dur + " days";
+}
 function updateFeed() {
     $.get('/backend/feed/',{},function(data) {
-        var html = "";
+        if(data['display']=="login")
+            window.location = "/frontend/login.html";
         for(tweetid in data['tweets']) {
             var tweet = data['tweets'][tweetid];
-            html += "<div class='tweet'>";
-            html += "<span class='user'>"+tweet['sourceuser']+"</span><br/>";
-            html += tweet['postcontent'];
-            html += "</div>";
+            if(!document.getElementById("feed-"+tweet['postid'])) {
+                tweet['sourceuser'] = JSON.parse(tweet['sourceuser']);
+                $("#feedContainer").prepend(_.template($("#tmpl-tweet").html(),{
+                    username: tweet['sourceuser']['username'],
+                    userid: tweet['sourceuser']['userid'],
+                    timestamp: tweet['timestamp'],
+                    tweet: tweet['postcontent'],
+                    tweetid: "feed-"+tweet['postid']
+                }));
+                $("#feedContainer :hidden").slideDown();
+            }
         }
-        $("#tweetsContainer").html(html);
+        refreshTimes();
     }, "json");
 }
-function showFeed() {
-    resizeIt();
-    $("#content-wrapper").html($("#tmpl-feed").html());
-}
-function updateProfile() {
-    $.get('/backend/myprofile/',{},function(data) {
-        var html = "";
 
-    });
-}
-function showProfile() {
-    $("#content-wrapper").html($("#tmpl-profile").html());
+function updateProfile(userid) {
+    if(arguments.length==0) url = '/backend/myprofile/';
+    else url = '/backend/profile/'+userid;
+    $.get(url,{},function(data) {
+        if(data['display']=="login")
+            window.location = "/frontend/login.html";
+        userid = data['userid'];
+        $("#profile-emailid").html(data['emailid']);
+        $("#profile-username").html(data['username']);
+        data['followerslist'] = JSON.parse(data['followerslist']);
+        data['followinglist'] = JSON.parse(data['followinglist']);
+        $("#profile-followers-count").html(data['followerslist'][0]['username']);
+        $("#profile-following-count").html(data['followinglist'][0]['username']);
+        for(tweetid in data['tweets']) {
+            var tweet = data['tweets'][tweetid];
+            if(!document.getElementById("tweet-"+tweet['postid'])) {
+                $("#tweetContainer").prepend(_.template($("#tmpl-tweet").html(),{
+                    username: data['username'],
+                    userid: data['userid'],
+                    timestamp: tweet['timestamp'],
+                    tweet: tweet['postcontent'],
+                    tweetid: "tweet-"+tweet['postid']
+                }));
+                $("#tweetContainer :hidden").slideDown();
+            }
+        }
+        refreshTimes();
+        $("#followButton").click(function() {
+            $.get('/backend/follow/'+userid,{},function(data) {
+                alert(data['message']);
+            }, "json");
+        });
+    },"json");
 }
 
 // Bootup code
 
+$("#tab-feed").addClass("active");
 updateFeed();
-showFeed();
-feedSetup();
